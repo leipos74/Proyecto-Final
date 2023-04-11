b5a311d17a45c9682a3319c45860dcfa49405099
PC-imatge-2022-2023 <licenses@enti.cat>
2023-04-11 11:18:06 +0200

    puertas parte da abajo

--- Assets/Scripts/Dungeon/UpController.cs
+++ Assets/Scripts/Dungeon/UpController.cs
@@ -19,17 +19,11 @@ public class UpController : MonoBehaviour
 
     }
 
-    public void OnTriggerStay2D(Collider2D other)
+    public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Pulsa e para entrar");
-            if (Input.GetKeyDown(KeyCode.E) && GameManager.Instance.key == true)
-            {
-                Debug.Log("Ahora puedes entrar");
-                SceneManager.LoadScene(Scene);
-            }
-
+            SceneManager.LoadScene(Scene);
         }
     }
 }
